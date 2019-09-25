656e7380f4d3ed19c12a0d88ec1e8f6055d7bcc6
Peter Ruzanov <pruzanov@gmail.com>
2019-09-25 16:39:20 -0400

    Changed calculate.sh

--- calculate.sh
+++ calculate.sh
@@ -4,9 +4,6 @@ set -o errexit
 set -o pipefail
 
 cd $1
-
-qrsh -l h_vmem=8G -cwd -now n "\
-module load samtools/1.9 2>/dev/null; \
-find . -regex '.*\.bam$' \
--exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo \" \; \
-| sort | uniq | tr '\t' '\n'"
+qrsh -V -l h_vmem=8G -cwd -now n "\
+find . -regex '.*\.WGS$' \
+       -exec sh -c \" wc -l {} | sed 's/ .*//' \" \; "
